af40a3b4761c2118dfe71b5896f0dab7cc0d2149
Hubbard Harvey <hcubedcoder@gmail.com>
2024-05-13 07:43:38 -0400

    Update csv, regenerate service client, add endpointID

--- names/names.go
+++ names/names.go
@@ -57,6 +57,7 @@ const (
 	ConfigServiceEndpointID              = "config"
 	DevOpsGuruEndpointID                 = "devops-guru"
 	ECREndpointID                        = "api.ecr"
+	EFSEndpointID                        = "elasticfilesystem"
 	EKSEndpointID                        = "eks"
 	EMREndpointID                        = "elasticmapreduce"
 	EventsEndpointID                     = "events"
